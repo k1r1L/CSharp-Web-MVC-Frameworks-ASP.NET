426a7ed56b8584f94915f78bfa17fb4fe8bf5060
Kiril Kirilov <kironakumura@abv.bg>
2017-04-04 19:52:01 +0300

    Filters exercises initial commit

---
+++ 04.-CSharp-Frameworks-ASPNET-Filters-Exercises/Filters.App/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿namespace Filters.App.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using Models;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            throw new Exception("Test exception message");
+        }
+
+        [HandleError(ExceptionType = typeof(ArgumentOutOfRangeException), View = "RangeError")]
+        [HandleError(ExceptionType = typeof(InvalidOperationException), View = "InvalidOperationError")]
+        public ActionResult Details(int id)
+        {
+            Car[] cars = new Car[]
+            {
+                new Car { Id=  1, Make = "Opel", Model = "Vectra", TravelledDistance = 3223423},
+                new Car { Id=  2, Make = "Dacia", Model = "Logan", TravelledDistance = 12323},
+                new Car { Id=  3, Make = "Mercedes", Model = "Benz", TravelledDistance = 123},
+                new Car { Id=  4, Make = "Porshe", Model = "Carrera", TravelledDistance = 567756757},
+            };
+
+            Car car = cars.SingleOrDefault(c => c.Id == id);
+            if (car == null)
+            {
+                throw  new ArgumentOutOfRangeException(nameof(id), id, "There is no such element with provided ID!");
+            }
+
+            if (car.TravelledDistance > 1000000)
+            {
+                throw new InvalidOperationException("The car is too old to be displayed");
+            }
+
+            ViewData["car"] = car;
+
+            return this.View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 04.-CSharp-Frameworks-ASPNET-Filters-Exercises/Filters.App/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace Filters.App.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public long TravelledDistance { get; set; }
+    }
+}
